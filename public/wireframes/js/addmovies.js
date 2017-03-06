dc2a20848230f1889abdee4980bb430fb1d1026a
Bibek Panday-kshetri <bpandayk@ucsd.edu>
2017-03-06 15:51:28 -0800

    logout/delete/update

--- public/wireframes/js/addmovies.js
+++ public/wireframes/js/addmovies.js
@@ -13,7 +13,8 @@ var addMovie=false;
 var search=false;
 var uploader;
 
-var myMovie = '<div class = "titles"><h3 class="act"><span class = "genre"> Action </span></h3>'+
+var myMovie = '<div class = "titles"><h3 class="act"><span class = "genre"> '+
+'Action </span></h3>'+
 '<div class = "action" id="Act">'+
     
 '</div><div class = "titles">'+
@@ -51,11 +52,7 @@ var addMovieD = ' <div class="second-level" id="secondlvl">' +
       '<div class="imag"><a href=" "><img src="images/12.jpg"></a></div>'+
       '<div class="imag"><a href=" "><img src="images/21.jpg"></a></div>'+
      ' <div class="imag"><a href=" "><img src="images/13.jpg"></a></div>'+
-     '<div class="rating">'+
-       '<span class="star">☆</span><span class="star">☆</span><span class="star">☆</span><span class="star">☆</span><span class="star5">☆</span>'+
-     '</div>' +
    '</div>'+
-     
    '<div class="col2">'+
     ' <div>'+
      ' <form>'+
@@ -127,11 +124,15 @@ var addMovieD = ' <div class="second-level" id="secondlvl">' +
   var user = localStorage.getItem("firebase:authUser:AIzaSyAupx69r_nEJqhkmzUAelKgZhPoguFrbXY:[DEFAULT]");
   var parser = JSON.parse(user);
   var UID = parser.uid;
+  var username = parser.displayName;
+  username = username.toUpperCase();
+  console.log(username);
 
-
+  
 
   
 function mybodyLoad() {
+  document.getElementById("dropmenu").innerHTML="Hi "+ username + "  !";
   var status = localStorage.getItem("current");
   if (status == "addMovie"){
      addMovieDom();
@@ -158,7 +159,7 @@ function getFormValues(){
 	rating=document.getElementById("rrate").value;
 	enjoyed=document.getElementById("enjoy").value;
 	description=document.getElementById("desc1").value;
-      uploader=document.getElementById("movimg").files;
+    uploader=document.getElementById("movimg").files;
 
 }
 
@@ -167,7 +168,6 @@ function getFormValues(){
 
 
 function addMovieDom() {
-	
     document.getElementById("sub-contain").innerHTML=addMovieD;
     if (typeof(Storage) !== "undefined"){
       localStorage.setItem("current", "addMovie");
@@ -316,13 +316,12 @@ var path = '/users/'+UID+'/'+detArray[1]+'/'+ detArray[0];
 
 
 	return firebase.database().ref(path).once('value').then(function(snapshot){
-    	    console.log(snapshot.val());
+
 		ret = snapshot.val();
 		var strin=" ";
 		var mname, mgenre,mrate, menjoy,mdate,mdesc,murl;
 		
 		if(ret) {  
-
 		  mname=ret.m_name; 
 		  mdate=ret.m_date; 
 		  mrate=ret.m_rating; 
@@ -338,23 +337,24 @@ var path = '/users/'+UID+'/'+detArray[1]+'/'+ detArray[0];
      '<div class="col1">'+
      '<img src='+murl+'>'+
      '<div class="rating">'+
-       '<span id="star0">☆</span><span id="star1">☆</span><span id="star2">☆</span><span id="star3">☆</span><span id="star4">☆</span>'+
+       '<span id="star0">☆</span><span id="star1">☆</span><span id="star2">☆</span>'+
+       '<span id="star3">☆</span><span id="star4">☆</span>'+
      '</div>'+
      '</div>'+
      
    '<div class="col2">'+
     ' <div class="name"> <span> '+mname+' </span></div>'+
      '<div class= "desc"> <p id="desc123">'+ mdesc+' </p>  </div>'+
-     '<div class= "genre"> <span class"=gen1">'+ mgenre +'</span> <span class="gen2"> 115min </span> </div>'+
+     '<div class= "genre"> <span class="gen1">'+ mgenre +'</span> <span class="gen2"> 115min </span> </div>'+
      '<div class= "refer"> <span> Enjoyed:&nbsp&nbsp&nbsp&nbsp' +menjoy+'</span> </div>'+
      '<div class= "date"> <span> Watched on:&nbsp&nbsp&nbsp&nbsp       '+ mdate +' </span> </div>'+
 
    '</div>'+
    '</div>'+
    '<div class="addsub">'+
-    '<input type="button" id="update" value="Update" onclick="updateData(\''+ path +'\')"></div>'+
+    '<input type="button" id="update" value="Update" onclick="updateData(\''+ mdetail+'\')"></div>'+
     '<div class="addsub">'+
-    '<input type="button" id = "del" value="Delete" onclick="deleteData(\''+ path +'\')"></div>'+
+    '<input type="button" id = "del" value="Delete" onclick="deleteData(\''+ mdetail+'\')"></div>'+
   '</div>';
 
 		 document.getElementById("sub-contain").innerHTML=viewdetail;
@@ -458,7 +458,7 @@ function submitData(){
        
          var fullUrl= path + '/' + imgname;
          	console.log(fullUrl);
-         firebase.database().ref(fullUrl).set({
+         return firebase.database().ref(fullUrl).set({
          	m_name: moviename,
          	m_date: date,
          	m_genre: genre,
@@ -467,6 +467,10 @@ function submitData(){
          	m_desc:description, 
          	m_url:downloadURL       
          });
+		}).then(function(snapshot){
+		    console.log("data added");
+		    myMovieDom();
+		
 		});
 		
 		}
@@ -517,4 +521,167 @@ function submitData(){
 	}
 }
 
+
+function deleteData(detail){
+    var detArray = detail.split("%");
+    var path = '/users/'+UID+'/'+detArray[1]+'/'+ detArray[0];
+    
+        console.log(detArray[0]);
+    
+    return firebase.database().ref(path).remove().then(function(snapshot){
+        console.log("success on delete");
+        var storage = firebase.storage().ref('images/'+detArray[0]);
+    	storage.delete().then(function(){
+        console.log("file deleted");
+        myMovieDom();   
+    });
+
+    }).catch(function(error){
+      console.log(error);
+    
+    });
+}
+
+
+
+function updateData(detail){
+	var name,date,enjoy,rate,url,desc;
+    var detArray = detail.split("%");
+    var path = '/users/'+UID+'/'+detArray[1]+'/'+ detArray[0];
+    
+	return firebase.database().ref(path).once('value').then(function(snapshot){
+
+		ret = snapshot.val();
+		var strin=" ";
+		var mname, mgenre,mrate, menjoy,mdate,mdesc,murl;
+		
+		if(ret) {  
+		  mname=ret.m_name; 
+		  mdate=ret.m_date; 
+		  mrate=ret.m_rating; 
+		  menjoy=ret.m_enjoy; 
+		  mgenre=ret.m_genre; 
+		  mdesc=ret.m_desc; 
+		  murl=ret.m_url; 
+		}    
+		
+		
+ var viewupdate = ' <div class = "movdetail" >'+
+  '<div><h2>'+mname+'</h2></div>'+
+   '<div class="second-level">'+
+     '<div class="col1">'+
+     '<img src='+murl+'>'+
+     '<div class="rating">'+
+       '<span id="star0">☆</span><span id="star1">☆</span><span id="star2">☆</span>'+
+       '<span id="star3">☆</span><span id="star4">☆</span>'+
+     '</div>'+
+     '</div>'+
+     
+   '<div class="col2">'+
+    ' <div>'+
+     ' <form>'+
+      '<label for="mname" id="l1">Movie Name</label>'+
+      '<input type="text" id="mname" name="moviename" placeholder="Latest movie you watched ...">'+
+
+      '<label for="date1" id="l2">When did you watch this movie?</label>'+
+      '<input type="date" name="wday" id="movdate">'+
+
+      '<label for="genre" id="l3">Genre</label>'+
+      '<select id="Genre" name="Genre">'+
+    '<option value="None" selected></option>'+
+		'<option value="Action">Action</option>'+
+		'<option value="Comedy">Comedy</option>'+
+		'<option value="Drama">Drama</option>'+
+		'<option value="Family&Kids">Family & Kids</option>'+
+		'<option value="Horror&Scifi">Horror & Scifi</option>'+
+		'<option value="Documentries">Documentries</option>'+
+    '</select>'+
+
+     ' <label for="rate" id="l4">Rating</label>'+
+      '<select id="rrate" name="Rate">'+
+    '<option value="0" selected></option>'+
+		'<option value="1">1</option>'+
+		'<option value="2">2</option>'+
+		'<option value="3">3</option>'+
+		'<option value="4">4</option>'+
+		'<option value="5">5</option>'+
+    '</select>'+
+
+    '<label for="enjoy" id="l5">Did you Enjoy the movie?</label>'+
+      '<select id="enjoy" name="enjoy">'+
+    '<option value="None"></option>'+
+		'<option value="yes">Yes</option>'+
+		'<option value="no">No</option>'+
+    '</select>'+
+
+    '<label for="desc" id="l6">Movie Description</label>'+
+    '<textarea rows="8"  id="desc1"> </textarea>'+
+
+    '<label for="movimg" id="l7">Movie Image </br></label>'+
+    '<input type="file" id="movimg" name="movimage" accept=".jpg,.jpeg,.png">'+
+    '<div class="addsub">'+
+    '<input type="button" value="Submit" onclick="submitupdateData()"></div>'+
+   '</form>'+
+   '</div>'+
+   '</div></div></div>';
+   
+		document.getElementById("sub-contain").innerHTML=viewupdate;
+		
+		if (typeof(Storage) !== "undefined"){
+      		localStorage.setItem("current", "viewdetail");
+      		localStorage.setItem("detail",mdetail);
+      	}
+		 
+		 if(mrate==1){
+		     document.getElementById("star0").style.color="gold";
+		     document.getElementById("star1").style.color="white";
+		     document.getElementById("star2").style.color="white";	
+		     document.getElementById("star3").style.color="white";
+		     document.getElementById("star4").style.color="white";   		     
+		 }
+		 
+		 if(mrate==2){
+		     document.getElementById("star0").style.color="gold";
+		     document.getElementById("star1").style.color="gold";
+		     document.getElementById("star2").style.color="white";	
+		     document.getElementById("star3").style.color="white";
+		     document.getElementById("star4").style.color="white";   		     
+		 }
+		 if(mrate==3){
+		     document.getElementById("star0").style.color="gold";
+		     document.getElementById("star1").style.color="gold";
+		     document.getElementById("star2").style.color="gold";	
+		     document.getElementById("star3").style.color="white";
+		     document.getElementById("star4").style.color="white";   		     
+		 }
+		 if(mrate==4){
+		     document.getElementById("star0").style.color="gold";
+		     document.getElementById("star1").style.color="gold";
+		     document.getElementById("star2").style.color="gold";	
+		     document.getElementById("star3").style.color="gold";
+		     document.getElementById("star4").style.color="white";   		     
+		 }
+		 if(mrate==5){
+		     document.getElementById("star0").style.color="gold";
+		     document.getElementById("star1").style.color="gold";
+		     document.getElementById("star2").style.color="gold";	
+		     document.getElementById("star3").style.color="gold";
+		     document.getElementById("star4").style.color="gold";   		     
+		 }		
+    });
+    
+     
+       
+}
+
+
+function logout(){
+	firebase.auth().signOut().then(function() {
+		window.location.href="index.html";
+	    console.log("logged out succesfully!");
+	}, function(error){
+	   console.log(error);
+	});
+
+}
 
